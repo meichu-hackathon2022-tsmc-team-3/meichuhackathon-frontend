bb673f67ed4b6301efcb80799c492a3b309cf009
kn71026 <nicolechou9712@gmail.com>
2022-10-23 11:27:08 +0800

    FIX: change footer

--- src/layout/dashboard/ContentFooter.vue
+++ src/layout/dashboard/ContentFooter.vue
@@ -3,14 +3,10 @@
     <div class="container-fluid">
       <ul class="nav">
         <li class="nav-item">
-          <a class="nav-link" href="http://www.creative-tim.com">
-            Creative Tim
-          </a>
+          <a class="nav-link" href="http://www.creative-tim.com"> TSMC </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="http://blog.creative-tim.com">
-            Blog
-          </a>
+          <a class="nav-link" href="http://blog.creative-tim.com"> BEST </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="http://www.creative-tim.com/license">
@@ -18,22 +14,26 @@
           </a>
         </li>
       </ul>
-      <div class="copyright">
-        © {{year}} made with <i class="tim-icons icon-heart-2"></i> by
-        <a href="https://www.binarcode.com" target="_blank" rel="noopener">Binar Code</a>&nbsp;&
-        <a href="https://www.creative-tim.com" target="_blank" rel="noopener">Creative Tim</a> for a better web.
-      </div>
+      <!-- <div class="copyright">
+        © {{ year }} made with <i class="tim-icons icon-heart-2"></i> by
+        <a href="https://www.binarcode.com" target="_blank" rel="noopener"
+          >Binar Code</a
+        >&nbsp;&
+        <a href="https://www.creative-tim.com" target="_blank" rel="noopener"
+          >Creative Tim</a
+        >
+        for a better web.
+      </div> -->
     </div>
   </footer>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        year: new Date().getFullYear()
-      }
-    }
-  };
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
 </script>
-<style>
-</style>
+<style></style>
