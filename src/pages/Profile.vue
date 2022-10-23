3a2afb0b0e6796c7ac98554fea2af0cb9de18733
kn71026 <nicolechou9712@gmail.com>
2022-10-23 11:46:24 +0800

    FIX: change event url

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="row">
     <div class="col-md-8">
-      <edit-profile-form :model="model">
-      </edit-profile-form>
+      <edit-profile-form :model="model"> </edit-profile-form>
     </div>
     <div class="col-md-4">
       <user-card :user="model"></user-card>
@@ -10,40 +9,46 @@
   </div>
 </template>
 <script>
-import EditProfileForm from './Profile/EditProfileForm';
-  import UserCard from './Profile/UserCard'
-  export default {
-    components: {
-      EditProfileForm,
-      UserCard
-    },
-    data() {
-      return {
-        model: {}
-      }
-    },
-    methods: {
-      async init(){
-        
-        const id = this.$route.query.id;
-        const event = (await this.$http.get(`${process.env.VUE_APP_EVENT_MANAGEMENT_URL}/event/id/${id}`)).data.detail;
-        const user = (await this.$http.get(`${process.env.VUE_APP_USER_MANAGEMENT_URL}/user/uid/${event.uid}`)).data.detail || {name: '', uid: ''}; 
+import EditProfileForm from "./Profile/EditProfileForm";
+import UserCard from "./Profile/UserCard";
+export default {
+  components: {
+    EditProfileForm,
+    UserCard,
+  },
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    async init() {
+      const id = this.$route.query.id;
+      const event = (
+        await this.$http.get(`http://event.tsmc.n0b.me/api/v1/event/id/${id}`)
+      ).data.detail;
+      const user = (
+        await this.$http.get(
+          `http://user.tsmc.n0b.me/api/v1/user/uid/${event.uid}`
+        )
+      ).data.detail || { name: "", uid: "" };
 
-        this.model = {
-          id: event._id,
-          url: "https://fakeimg.pl/250/",
-          result: event.result == 'yellow' ? `${user.name} 在進入施工區未著裝完成` : "偵測到有人在施工區未戴安全帽",
-          time: new Date(event.time).toLocaleString()
-        }
+      this.model = {
+        id: event._id,
+        url: event.url,
+        result:
+          event.result == "yellow"
+            ? `${user.name} 在進入施工區未著裝完成`
+            : "偵測到有人在施工區未戴安全帽",
+        time: new Date(event.time).toLocaleString(),
+      };
 
-        console.log(this.model);
-
-      }
+      console.log(this.model);
     },
-    created() {
-      this.init();
-    }
-  }
+  },
+  created() {
+    this.init();
+  },
+};
 </script>
-<style>
-</style>
+<style></style>
